91333dd0155ec6f8b45e62e05fc6ce2ce3915901
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-08-28 23:41:23 +0500

    Shop Page & Home Page Settiings

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,13 @@ jQuery(document).ready(function ($) {
     buttons: false,
   });
 });
+$("#filter-image").on("click", function () {
+  var $sidebar = $("#sidebar");
+  var $content = $("#content");
+
+  // Toggle the visibility of the sidebar and adjust the content width
+  $sidebar.toggleClass("d-none col-md-3 col-lg-3 col-xl-3");
+  $content.toggleClass(
+    "col-md-9 col-lg-9 col-xl-9 col-md-12 col-lg-12 col-xl-12"
+  );
+});
