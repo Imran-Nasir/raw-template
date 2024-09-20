e1358e176440b563e4efae7a4189be78b6f10144
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-09-20 14:20:06 +0500

    Merge Conflicts

--- assets/js/main.js
+++ assets/js/main.js
@@ -302,6 +302,44 @@ jQuery(document).ready(function ($) {
       buttons: false,
     });
   }
+  if ($(".clients-slider").length) {
+    $(".clients-slider").slick({
+      dots: false,
+      arrows: false,
+      infinite: true,
+      speed: 3000,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
+    });
+  }
 });
 //shop pagination
 $(document).ready(function () {
